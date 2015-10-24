f509f90469d4a82196c5b06293e392d35f3d118b
Alex Stophel <alexstophel@gmail.com>
2015-10-24 15:49:46 -0400

    Add basic config method.

--- lib/compadre.rb
+++ lib/compadre.rb
@@ -1,4 +1,15 @@
 require "compadre/engine"
 
 module Compadre
+  def self.configure
+    yield self
+  end
+
+  def self.resource_name
+    (@resource_name || "user").split("_").map(&:capitalize).join("")
+  end
+
+  def self.resource_name=(resource_name)
+    @resource_name = resource_name
+  end
 end
