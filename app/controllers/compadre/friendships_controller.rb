cccd3fbf75c0efd34fc82f52f514de644faaca0f
JessePledger <JessePledger@gmail.com>
2015-10-25 19:11:12 -0400

    Created friendship and friend_requests controllers.

---
+++ app/controllers/compadre/friendships_controller.rb
@@ -0,0 +1,6 @@
+require_dependency "compadre/application_controller"
+
+module Compadre
+  class FriendshipsController < ApplicationController
+  end
+end
---
+++ spec/controllers/compadre/friendships_controller_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+module Compadre
+  RSpec.describe FriendshipsController, type: :controller do
+
+  end
+end
